700496d84b745a9fdc34b3a6d8a4e49c98cf8372
Chen-Pang He <chen.pang.he@jdh8.org>
2026-08-21 05:18:45 +0800

    Update copyright years

--- benches/predicates.rs
+++ benches/predicates.rs
@@ -1,6 +1,6 @@
 // This file is part of the minifloat project.
 //
-// Copyright (C) 2025 Chen-Pang He <jdh8@skymizer.com>
+// Copyright (C) 2025-2026 Chen-Pang He <jdh8@skymizer.com>
 //
 // This Source Code Form is subject to the terms of the Mozilla
 // Public License v. 2.0. If a copy of the MPL was not distributed
